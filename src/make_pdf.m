5a2f2a3c6193368877eea1e6d8ff56fbb7edf339
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2019-01-18 16:05:51 -0600

    Working in Matlab IDE

--- src/make_pdf.m
+++ src/make_pdf.m
@@ -4,15 +4,31 @@ function [pdf_file] = make_pdf(t1_nii,meanfmri_nii,rp_txt,out_dir, ...
 % We'll use SPM functions to show the mean fmri. We make an invisible
 % figure and print to PDF. Hopefully this works in combination with
 % -nodisplay -nodesktop -nosplash command line options for the matlab call.
+%
+% This is pretty ugly because I threw it together fast. For a real spider,
+% it's worth spending a little time to make a more thorough and informative
+% report page (or multiple pages). Tools from ImageMagick can be useful to
+% stitch together multiple PNGs or multiple pages.
 
+% Initialize the figure
 F = figure('visible','off');
+colormap(gray);
 
+% Show SPM's three-pane view
 H = spm_orthviews('Image',meanfmri_nii,[],F);
 
-infostring = [xnat_project ' ' xnat_subject ' ' xnat_session ': ' ...
-	'T1 ' xnat_scan_t1 ', fMRI ' xnat_scan_fmri];
-spm_orthviews('Caption',H,infostring);
+% Make a two-line title string for the figure using a cell array
+infostring = {[xnat_project ' ' xnat_subject ' ' xnat_session], ...
+	['T1 ' xnat_scan_t1 ', fMRI ' xnat_scan_fmri]};
 
+% Escape the _ so matlab doesn't interpret as subscript
+infostring = strrep(infostring,'_','\_');
+
+% Put the string onto the upper right panel of SPM's figure
+A = get(F,'Children');
+A(2).Title.String = infostring;
+
+% Print to file
 pdf_file = fullfile(out_dir,'report.pdf');
 print(F,'-dpdf',pdf_file);
 
---
+++ src/test_matlab.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+#
+# Test matlab in command line mode.
+matlab -nodisplay -nodesktop -nosplash -r 'test_matlab'
